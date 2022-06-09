793125ba422556cee137b288900666f177057c01
rpm-16 <rpm-16@mail.ru>
2022-06-09 22:31:44 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState('text');
+
+  function increment(){
+    setLikes( likes + 1 );
+  }
+
+  function decrement(){
+    setLikes( likes - 1 );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes} {value}</h1>
+      Приложение
+      <input type='text' 
+        value={value} 
+        onChange = {event => setValue(event.target.value)}
+        />
+      <button onClick = {increment}> pls </button>
+      <button onClick = {decrement}> minus </button>
     </div>
   );
+
 }
 
 export default App;
